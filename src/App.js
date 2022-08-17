dd71470ff166f00a61b097642e831e23af51c747
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-17 13:01:17 +0200

    slette unødvendig fil

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from './components/Header';
 import Map from './components/Map';
 
-import css from './App.module.css';
-
 function App() {
   return (
-    <main className={css.app}>
+    <main>
       <Header />
       <Map />
     </main>
