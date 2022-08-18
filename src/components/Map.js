c6db19200883709a1a02acabc025845877914d0a
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-18 02:08:03 +0200

    ferdig:)

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,18 +2,19 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import css from './Map.module.css';
 
-function Map() {
+function Map(props) {
+  const { lat: latitude, lng: longitude } = props.location.location;
+  // console.log(latitude, longitude);
+
   return (
     <div className={css.map}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={[latitude, longitude]}>
+          <Popup>:)</Popup>
         </Marker>
       </MapContainer>
     </div>
