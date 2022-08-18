c0e60c28a03b9f5c185f04f7ffdfd6a7d1399595
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-18 18:02:38 +0200

    lagt til passing på input-field

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,32 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 import css from './Map.module.css';
 
 function Map(props) {
-  const { lat: latitude, lng: longitude } = props.location.location;
-  // console.log(latitude, longitude);
+  const [hasCoords, setHasCoords] = useState(false);
+
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+
+  useEffect(() => {
+    setLat(props.location.latitude);
+    setLng(props.location.longitude);
+    setHasCoords(true);
+  }, [props]);
+
+  console.log(lat, lng);
 
   return (
     <div className={css.map}>
-      <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={[latitude, longitude]}>
-          <Popup>:)</Popup>
-        </Marker>
-      </MapContainer>
+      {hasCoords && (
+        <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+        </MapContainer>
+      )}
     </div>
   );
 }
