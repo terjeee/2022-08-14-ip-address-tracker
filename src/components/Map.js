f63fcf5625f94811cd6ed29174a247632f04b9db
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-19 03:07:55 +0200

    endre coords funker, fjernet infinite loop

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,32 @@
-import { useState, useEffect } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 import css from './Map.module.css';
 
+// ChangeCoords endrer location på kartet gjennom map.setVie() da MapContainer (original render av map) er immutable - https://stackoverflow.com/questions/64665827/react-leaflet-center-attribute-does-not-change-when-the-center-state-changes
+const ChangeCoords = ({ center, zoom }) => {
+  const map = useMap();
+  map.flyTo(center, zoom);
+  return null;
+};
+
 function Map(props) {
   const [hasCoords, setHasCoords] = useState(false);
-
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
+  const [coords, setCoords] = useState([null, null]);
 
   useEffect(() => {
-    setLat(props.location.latitude);
-    setLng(props.location.longitude);
-    setHasCoords(true);
+    setCoords([props.location.latitude, props.location.longitude]);
   }, [props]);
 
-  console.log(lat, lng);
+  useEffect(() => {
+    if (coords[0] !== null && coords[1] !== null) setHasCoords(true);
+  }, [coords]);
 
   return (
     <div className={css.map}>
       {hasCoords && (
-        <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={coords} zoom={13} scrollWheelZoom={true} keyboard={true}>
+          <ChangeCoords center={coords} zoom={12} />
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
