dcbb063c54a3ee98d9dc0f56054c416d191b7d7a
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-20 02:59:04 +0200

    refactor Map.js (unødvendig kode o rerenders)

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 import css from './Map.module.css';
@@ -11,37 +10,17 @@ const ChangeCoords = ({ center, zoom }) => {
 };
 
 function Map(props) {
-  const [hasCoords, setHasCoords] = useState(false);
-  const [coords, setCoords] = useState([null, null]);
-
-  useEffect(() => {
-    setHasCoords(false);
-    setCoords([props.location.latitude, props.location.longitude]);
-  }, [props]);
-
-  useEffect(() => {
-    if (
-      coords[0] !== null &&
-      coords[1] !== null &&
-      coords[0] !== undefined &&
-      coords[1] !== undefined
-    )
-      return setHasCoords(true);
-  }, [coords]);
-
-  console.log('MAP');
+  const coords = [props.location.latitude, props.location.longitude];
 
   return (
     <div className={css.map}>
-      {hasCoords && (
-        <MapContainer center={coords} zoom={13} scrollWheelZoom={false} keyboard={true}>
-          <ChangeCoords center={coords} zoom={13} />
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          />
-        </MapContainer>
-      )}
+      <MapContainer center={coords} zoom={13} scrollWheelZoom={false} keyboard={true}>
+        <ChangeCoords center={coords} zoom={13} />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
     </div>
   );
 }
